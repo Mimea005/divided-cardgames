c111679bf715e324652e9a89b1dd6c2e20ca2bb2
Mimea005 <michael.mendes1@gmail.com>
2021-10-14 15:57:48 +0200

    Converted js to ts

--- source/modules/Display.ts
+++ source/modules/Display.ts
@@ -1,1 +1,42 @@
-// TODO Write everything!
+import { Card } from "modules/Card";
+
+function drawCard(card: Card): string {
+    return /* html */`
+        <div class="card">
+            <div class="card-row card-top">
+                <p class="card-symbol">${card.suit.symbol} ${card.symbol}</p>
+                <p class="card-symbol">${card.suit.symbol} ${card.symbol}</p>
+            </div>
+        </div>
+    `;
+}
+
+function drawCardBack() {
+    return /* html */ `
+        <div class="card card-back"></div>
+    `;
+}
+
+function drawCards(cards: Array<Card>, visible?: Array<number>): string {
+    let result = "";
+
+    if (!visible) {
+        for (let card of cards) {
+            result += drawCard(card);
+        }
+    } else {
+        if (visible.length < cards.length) {
+            for (let i = 0; i < (cards.length - visible.length); i++) {
+                visible.push(1);
+            }
+        }
+
+        for (let i in cards) {
+            visible[i] ? result += drawCard(cards[i]) : result += drawCardBack();
+        }
+    }
+
+    return result;
+}
+
+export { drawCard, drawCardBack, drawCards }
--- source/modules/Random.ts
+++ source/modules/Random.ts
@@ -1,1 +1,14 @@
-// TODO Write everything!
+function random(max: number, min: number = 0): number {
+    let result: number;
+    result = Math.random() * (max - min);
+    result = Math.floor(result + min);
+    return result;
+}
+
+function randomPop(choices: Array<any>): any {
+    let rIndex = random(choices.length);
+    let result = choices.splice(rIndex, 1);
+    return result;
+}
+
+export { random, randomPop }
